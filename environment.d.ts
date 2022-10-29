5e5cb6a21e16a6572fa100833a30a6f0232f56d4
Nick Fields <devNickF@gmail.com>
2022-10-28 21:20:04 -0700

    define .env types

---
+++ environment.d.ts
@@ -0,0 +1,15 @@
+declare global {
+  namespace NodeJS {
+    /**
+     * .env type definitions
+     * @link https://stackoverflow.com/questions/45194598/using-process-env-in-typescript
+     */
+    interface ProcessEnv {
+      APP_URL: string;
+    }
+  }
+}
+
+// If this file has no import/export statements (i.e. is a script)
+// convert it into a module by adding an empty export statement.
+export {};
